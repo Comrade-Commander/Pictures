b65583c4d4829c4c87435fd097056f87c3615bff
Comrade-Commander <84791666+Comrade-Commander@users.noreply.github.com>
2021-12-06 18:34:50 +0800

    Update random.php

--- got/random.php
+++ got/random.php
@@ -1,6 +1,6 @@
 <?php
 $seed=time();
-$num=rand(1,100)
+$num=rand(1,113);
 $picpath="https://github.com/Comrade-Commander/pictures/blob/main/got/".$num."jpg";
 die(header("Location: $picpath"));
 ?>
